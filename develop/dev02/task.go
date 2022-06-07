35c4e0427908ded4ec51f133398e260e0086d932
ion_mion <solovev.ma18@physics.msu.ru>
2022-06-07 16:26:50 +0300

    +dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +26,87 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Unpacker interface {
+	Unpack() (string, error)
+}
 
+type PackedString string
+
+func (s PackedString) Unpack() (string, error) {
+	var lastRune, lastLetter rune
+	var result, num strings.Builder
+	var esc bool
+
+	result.Reset()
+	num.Reset()
+
+	lastRune = 0
+	lastLetter = 0
+
+	for i, curRune := range s {
+		// if first character is num, wrong input
+		if i == 0 && unicode.IsDigit(curRune) {
+			return "", errors.New("error: first character is digit")
+		}
+		// writing to result and unpacking previous sequence
+		if unicode.IsLetter(curRune) {
+			// if letter after digit
+			if unicode.IsDigit(lastRune) {
+				numRunes, err := strconv.Atoi(num.String())
+				if err != nil {
+					return "", err
+				}
+				for j := 0; j < numRunes-1; j++ {
+					result.WriteRune(lastLetter)
+				}
+				num.Reset()
+			}
+			result.WriteRune(curRune)
+			lastLetter = curRune
+			lastRune = curRune
+		}
+		// write to num or write digit to result
+		if unicode.IsDigit(curRune) {
+			// escape sequence
+			if esc {
+				result.WriteRune(curRune)
+				lastLetter = curRune
+				lastRune = curRune
+				esc = false
+			} else {
+				// if digit after letter
+				if unicode.IsLetter(lastRune) {
+					num.Reset()
+				}
+				num.WriteRune(curRune)
+				lastRune = curRune
+				// last digit in input string
+				if i == utf8.RuneCountInString(string(s))-1 {
+					numRunes, err := strconv.Atoi(num.String())
+					if err != nil {
+						return "", err
+					}
+					for j := 0; j < numRunes-1; j++ {
+						result.WriteRune(lastLetter)
+					}
+				}
+			}
+		}
+		if curRune == '\\' {
+			if esc {
+				result.WriteRune(curRune)
+				lastLetter = curRune
+				lastRune = curRune
+				esc = false
+			} else {
+				if i == utf8.RuneCountInString(string(s))-1 {
+					return "", errors.New("error: last character is \\")
+				}
+				esc = true
+				lastRune = curRune
+			}
+		}
+	}
+
+	return result.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,92 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var tests = []struct {
+	name    string
+	isValid bool
+	ps      PackedString
+	want    string
+}{
+	{
+		name:    "valid-001",
+		isValid: true,
+		ps:      "a4bc2d5e",
+		want:    "aaaabccddddde",
+	},
+	{
+		name:    "valid-002",
+		isValid: true,
+		ps:      "abcd",
+		want:    "abcd",
+	},
+	{
+		name:    "not valid-001",
+		isValid: false,
+		ps:      "45",
+		want:    "",
+	},
+	{
+		name:    "valid-003",
+		isValid: true,
+		ps:      "",
+		want:    "",
+	},
+	{
+		name:    "valid-004",
+		isValid: true,
+		ps:      "qwe\\4\\5",
+		want:    "qwe45",
+	},
+	{
+		name:    "valid-005",
+		isValid: true,
+		ps:      "qwe\\45",
+		want:    "qwe44444",
+	},
+	{
+		name:    "valid-006",
+		isValid: true,
+		ps:      "qwe\\\\5",
+		want:    "qwe\\\\\\\\\\",
+	},
+	{
+		name:    "not valid-002",
+		isValid: false,
+		ps:      "\\\\\\",
+		want:    "",
+	},
+	{
+		name:    "not valid-003",
+		isValid: false,
+		ps:      "a\\\\4\\",
+		want:    "",
+	},
+}
+
+func TestUnpack(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.isValid == true {
+				res, err := test.ps.Unpack()
+				if err != nil {
+					t.Fatal(err)
+				}
+				if res != test.want {
+					t.Fatal(fmt.Sprintf("Result: %s Want: %s", res, test.want))
+				}
+			} else {
+				res, err := test.ps.Unpack()
+				if err == nil {
+					t.Fatal(err)
+				}
+				if res != test.want {
+					t.Fatal(fmt.Sprintf("Result: %s Want: %s", res, test.want))
+				}
+			}
+		})
+	}
+}
