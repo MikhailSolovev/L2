eb3e9a879e9a6a0b9dd9c894b0bfcdd9785ebce0
ion_mion <solovev.ma18@physics.msu.ru>
2022-06-08 12:32:54 +0300

    +dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +21,90 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func Min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
 
+// RunCut - нарезает строку по разделителю
+// Use: runCut([]string{"-s" `-d"[delim]"` `-f[start-stop]` `"[string]"`})
+// Поддерживает флаги:
+// -f - "fields" - выбрать поля (колонки)
+// -d - "delimiter" - использовать другой разделитель
+// -s - "separated" - только строки с разделителем
+func RunCut(args []string) string {
+	data := ""
+	suppress := false
+	delim := "\t"
+	var start, stop int64
+	var err error
+
+	// Проверки на количество аргументов
+	if len(args) > 4 {
+		return fmt.Sprintf("cut: too much args")
+	}
+	if len(args) < 2 {
+		return fmt.Sprintf("cut: too less args")
+	}
+
+	for _, arg := range args {
+		switch {
+		// Парсим "[string]"
+		case regexp.MustCompile(`(^").*("$)`).MatchString(arg):
+			data = strings.TrimSuffix(arg, `"`)[1:]
+		// Парсим -d"[delim]"
+		case regexp.MustCompile(`-d".*"`).MatchString(arg):
+			delim = strings.TrimSuffix(arg, `"`)[3:]
+		// Парсим ключ -s
+		case arg == "-s":
+			suppress = true
+		// Парсим -f[start-stop]
+		case regexp.MustCompile(`-f.*`).MatchString(arg):
+			temp := arg[2:]
+			if regexp.MustCompile(`^\d+$`).MatchString(temp) {
+				start, err = strconv.ParseInt(temp, 10, 64)
+				if err != nil {
+					return fmt.Sprintf("cut: %s", err.Error())
+				}
+				stop = start
+			} else if regexp.MustCompile(`^\d+-$`).MatchString(temp) {
+				temp = strings.TrimSuffix(temp, `-`)
+				start, err = strconv.ParseInt(temp, 10, 64)
+				if err != nil {
+					return fmt.Sprintf("cut: %s", err.Error())
+				}
+				stop = math.MaxInt64
+			} else if regexp.MustCompile(`^-\d+$`).MatchString(temp) {
+				temp = temp[1:]
+				start = 1
+				stop, err = strconv.ParseInt(temp, 10, 64)
+				if err != nil {
+					return fmt.Sprintf("cut: %s", err.Error())
+				}
+			} else if regexp.MustCompile(`^\d+-\d+$`).MatchString(temp) {
+				start, err = strconv.ParseInt(strings.Split(temp, "-")[0], 10, 64)
+				if err != nil {
+					return fmt.Sprintf("cut: %s", err.Error())
+				}
+				stop, err = strconv.ParseInt(strings.Split(temp, "-")[1], 10, 64)
+				if err != nil {
+					return fmt.Sprintf("cut: %s", err.Error())
+				}
+			}
+		// Если отдан какой-то другой аргумент
+		default:
+			return fmt.Sprintf("cut: illegal option or input %s", arg)
+		}
+	}
+
+	res := strings.Split(data, delim)
+
+	// Если строка не имеет заданного разделителя, то мы ее выкидываем
+	if suppress && len(res) == 1 {
+		return ""
+	}
+
+	return strings.Join(res[Min(start-1, int64(len(res))):Min(stop, int64(len(res)))], delim)
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var tests = []struct {
+	name string
+	args []string
+	want string
+}{
+	{
+		name: "test-001",
+		args: []string{"-f4", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "cow",
+	},
+	{
+		name: "test-002",
+		args: []string{"-f-3", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "cat\tdog\thorse",
+	},
+	{
+		name: "test-003",
+		args: []string{"-f3-", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "horse\tcow\tduck\telephant\tchicken\tparrot",
+	},
+	{
+		name: "test-004",
+		args: []string{"-f2-5", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "dog\thorse\tcow\tduck",
+	},
+	{
+		name: "test-005",
+		args: []string{`-d" "`, "-f2-5", "\"cat dog horse cow duck elephant chicken parrot\""},
+		want: "dog horse cow duck",
+	},
+	{
+		name: "test-006",
+		args: []string{`-d" "`, "-s", "-f2-5", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "",
+	},
+	{
+		name: "test-008",
+		args: []string{"-s", "-f10-12", "\"cat\tdog\thorse\tcow\tduck\telephant\tchicken\tparrot\""},
+		want: "",
+	},
+}
+
+func TestRunCut(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res := RunCut(test.args)
+			if res != test.want {
+				t.Fatal(fmt.Sprintf("Result: %s Want: %s", res, test.want))
+			}
+		})
+	}
+}
